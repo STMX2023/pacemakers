bb7ade7b4c1254b83a8560a6885d5aa40dc5a44d
STMX2023 <124530534+STMX2023@users.noreply.github.com>
2025-03-16 11:41:54 +0200

    Set default name and slug for Expo configuration

--- client/app.config.ts
+++ client/app.config.ts
@@ -2,6 +2,8 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
+  name: config.name || "Pacemakers",
+  slug: config.slug || "pacemakers",
   android: {
     ...config.android,
     package: "com.x779wv3.client"
